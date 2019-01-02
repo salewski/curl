4dcc400f6a51f55c8270b71c7d0fb2c1b5c6ba15
Daniel Stenberg <daniel@haxx.se>
2019-01-02 18:04:58 +0100

    urldata: rename easy_conn to just conn
    We use "conn" everywhere to be a pointer to the connection.
    
    Introduces two functions that "attaches" and "detaches" the connection
    to and from the transfer.
    
    Going forward, we should favour using "data->conn" (since a transfer
    always only has a single connection or none at all) to "conn->data"
    (since a connection can have none, one or many transfers associated with
    it and updating conn->data to be correct is error prone and a frequent
    reason for internal issues).
    
    Closes #3442

--- lib/urldata.h
+++ lib/urldata.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -1777,7 +1777,7 @@ struct Curl_easy {
   struct Curl_easy *next;
   struct Curl_easy *prev;
 
-  struct connectdata *easy_conn;     /* the "unit's" connection */
+  struct connectdata *conn;
   struct curl_llist_element connect_queue;
   struct curl_llist_element pipeline_queue;
 
